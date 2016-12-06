0f0f5b1d676e8aa04e556875bec593210232e0d2
mrpancham97 <talktopancham@gmail.com>
2016-12-06 17:57:41 +0530

    Twelfth commit with Team page

--- js/style.js
+++ js/style.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
       //.log to determine when you want the
       //nav bar to stick.
     //  console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 185) {
+    if ($(window).scrollTop() > 215) {
       $('#nav_bar').addClass('navbar-fixed');
       $("#nav").addClass('navbar-fixed2');
       $("#down").hide();
     }
-    if ($(window).scrollTop() <= 200) {
+    if ($(window).scrollTop() <= 218) {
       $('#nav_bar').removeClass('navbar-fixed');
       $("#nav").removeClass('navbar-fixed2');
       $("#down").show();
